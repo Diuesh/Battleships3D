f13c83615db653f754d0efabc43f084247996587
deeaeu <gabriel.stanila@student.upt.ro>
2021-05-06 02:18:19 +0300

    Revert "Revert "selectie plan""
    This reverts commit f8747103a6671abecf85568b78e2da2ebd5562d8.

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from panda3d.core import loadPrcFile
+from direct.gui.DirectGui import *
+from panda3d.core import *
+from direct.showbase.ShowBase import ShowBase
 loadPrcFile("config/conf.prc")
 
-from direct.showbase.ShowBase import ShowBase
+v = [1]  # Valoarea initiala (default)
+bk_text = "selector plan"
+
+
+def settext(status=None):
+    bk_text = "Valoarea curenta: %s" % v
+
+
+buttons = [DirectRadioButton(text='Submarine', variable=v, value=[0], scale=0.05, pos=(1, 0, -0.2), command=settext()),
+           DirectRadioButton(text='Barci', variable=v, value=[1], scale=0.05, pos=(1, 0, 0), command=settext()),
+           DirectRadioButton(text='Avioane', variable=v, value=[2], scale=0.05, pos=(1, 0, 0.2), command=settext())]
+for button in buttons:
+    button.setOthers(buttons)
+
 
 class Bship(ShowBase):
     def __init__(self):
         super().__init__()
 
-        #disable camera
-        #self.disable_mouse()
+        # disable camera
+        # self.disable_mouse()
 
         box = self.loader.loadModel("models/box")
         box.setPos(0, 10, 0)
         box.reparentTo(self.render)
-
-
-
-
 
 
 game = Bship()
