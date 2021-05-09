c89e1e90ce143183af6750d3c720f768aff6f9b7
Diuesh <rapeterea@gmail.com>
2021-05-09 19:08:13 +0300

    Fixed camera Added AI file

--- Battleship.py
+++ Battleship.py
@@ -1,5 +1,8 @@
+from direct.gui.DirectButton import DirectButton
+from direct.task.Task import Task
+from direct.task.TaskManagerGlobal import taskMgr
 from panda3d.core import loadPrcFile
-
+import keyboard
 
 loadPrcFile("config/conf.prc")
 
@@ -10,15 +13,90 @@ class Bship(ShowBase):
     def __init__(self):
         super().__init__()
 
-        #disable camera
-        #self.disable_mouse()
-
         #camera settup
-        # self.camera.setPos(-10, -10, -10)
-        # self.camera.setHpr(0, 0, 0)
+        self.camera.setHpr(-35, -24, 0)
+        self.camera.setPos(-2.5, -4, 7)
+
+        #disable camera
+        self.disable_mouse()
 
         #env settup
         self.setBackgroundColor(0.1, 0.6, 1.0)
+
+
+
+        #TaskManager
+        taskMgr.add(self.movCameraTask, 'movCameraTask')
+
+    def movCameraTask(self, task):
+
+        pos = self.camera.getPos()
+        if keyboard.is_pressed('1'):
+            print("Boat")
+            pos.z = 7
+        if keyboard.is_pressed('2'):
+            print("Submarine")
+            pos.z = -7
+        if keyboard.is_pressed('3'):
+            print("Plane")
+            pos.z = 21
+        self.camera.setPos(pos)
+
+        return Task.cont
+
+
+    #Entities
+
+    def corners_b(self, x, y):
+        box = self.loader.loadModel("models/box")
+        box.setPos(x, y, 0)
+        box.reparentTo(self.render)
+
+        box = self.loader.loadModel("models/box")
+        box.setPos(x + 9, y, 0)
+        box.reparentTo(self.render)
+
+        box = self.loader.loadModel("models/box")
+        box.setPos(x, y + 9, 0)
+        box.reparentTo(self.render)
+
+        box = self.loader.loadModel("models/box")
+        box.setPos(x + 9, y + 9, 0)
+        box.reparentTo(self.render)
+
+    def corners_a(self, x, y, z):
+        box = self.loader.loadModel("models/box")
+        box.setPos(x, y, z)
+        box.reparentTo(self.render)
+
+        box = self.loader.loadModel("models/box")
+        box.setPos(x + 9, y, z)
+        box.reparentTo(self.render)
+
+        box = self.loader.loadModel("models/box")
+        box.setPos(x, y + 9, z)
+        box.reparentTo(self.render)
+
+        box = self.loader.loadModel("models/box")
+        box.setPos(x + 9, y + 9, z)
+        box.reparentTo(self.render)
+
+    def corners_s(self, x, y, z):
+        box = self.loader.loadModel("models/box")
+        box.setPos(x, y, z)
+        box.reparentTo(self.render)
+
+        box = self.loader.loadModel("models/box")
+        box.setPos(x + 9, y, z)
+        box.reparentTo(self.render)
+
+        box = self.loader.loadModel("models/box")
+        box.setPos(x, y + 9, z)
+        box.reparentTo(self.render)
+
+        box = self.loader.loadModel("models/box")
+        box.setPos(x + 9, y + 9, z)
+        box.reparentTo(self.render)
 
     def cruiser(self, x, y):
         box = self.loader.loadModel("models/box")
