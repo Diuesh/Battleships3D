453ddf4638a65650e7cb07d88c6a653a74e109e4
Diuesh <rapeterea@gmail.com>
2021-05-12 15:55:47 +0300

    Updated requierments Implemented Player selection

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ from Battleship import *
 
 game = Bship()
 def PlayerArena():
-    game.cruiser(3, 4)
-    game.boat(5, 6)
-    game.boat(1, 5)
-    game.submarine(2, 4, -14)
-    game.plane(3, 4, 14)
-    game.corners_b(1, 4)
-    game.corners_a(1, 4, 14)
-    game.corners_s(1, 4, -14)
+    game.corners_b()
+    game.corners_a()
+    game.corners_s()
+    game.pieces_settup()
+    game.run()
 
 def AIArena():
     pass
+
 PlayerArena()
-game.run()
+
 
 
 
